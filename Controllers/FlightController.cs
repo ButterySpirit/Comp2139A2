d93e4fe4705ebc500d31ec2101b8a11e1cf82744
IanM21 <ianmcdonad2003@gmail.com>
2024-02-04 16:16:55 -0500

    Added Hotels

---
+++ Controllers/FlightController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assign1.Controllers
+{
+    public class FlightController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
