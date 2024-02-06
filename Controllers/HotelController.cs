9a9ef358d14be789e4e243f34ba51094a560427b
IanM21 <ianmcdonad2003@gmail.com>
2024-02-06 10:11:47 -0500

    Removed Hotel Form & POST in Controller

--- Controllers/HotelController.cs
+++ Controllers/HotelController.cs
@@ -27,37 +27,5 @@ public class HotelController : Controller
         }
         return View(hotel);
     }
-
-    // POST: Hotel/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("HotelId,HotelName,Address,City,PostalCode,State,Country,Status,StarRating,RoomType,NumberOfRooms,MaxOccupancy")] Hotel hotel)
-    {
-        if (ModelState.IsValid)
-        {
-            var addHotel = new Hotel
-            {
-                HotelId = hotel.HotelId,
-                HotelName = hotel.HotelName,
-                Address = hotel.Address,
-                City = hotel.City,
-                PostalCode = hotel.PostalCode,
-                State = hotel.State,
-                Country = hotel.Country,
-                CostPerNight = hotel.CostPerNight,
-                Status = hotel.Status,
-                StarRating = hotel.StarRating,
-                RoomType = hotel.RoomType,
-                NumberOfRooms = hotel.NumberOfRooms,
-                MaxOccupancy = hotel.MaxOccupancy
-            };
-
-            _context.Add(hotel);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-        return View(hotel);
-    }
-
 }
 
