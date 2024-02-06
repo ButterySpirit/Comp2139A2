83f7b5bed1ece1046bdf82f945b5e92a3ac8fe1b
IanM21 <ianmcdonad2003@gmail.com>
2024-02-05 20:37:24 -0500

    i dont even know whats going on anymore.

--- Controllers/HotelController.cs
+++ Controllers/HotelController.cs
@@ -1,20 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Assign1.Data;
+using Assign1.Models;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace Assign1.Controllers
+public class HotelController : Controller
 {
-    public class HotelController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public HotelController(ApplicationDbContext context)
     {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _context = context;
     }
+
+    // GET: Hotel/Create
+    public IActionResult Index()
+    {
+        var hotel = _context.Hotels.ToList();
+        return View(hotel);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var hotel = _context.Hotels.FirstOrDefault(p => p.HotelId == id);
+        if (hotel == null)
+        {
+            return NotFound();
+        }
+        return View(hotel);
+    }
+
+    // POST: Hotel/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("HotelId,HotelName,Address,City,PostalCode,State,Country,Status,StarRating,RoomType,NumberOfRooms,MaxOccupancy")] Hotel hotel)
+    {
+        if (ModelState.IsValid)
+        {
+            var addHotel = new Hotel
+            {
+                HotelId = hotel.HotelId,
+                HotelName = hotel.HotelName,
+                Address = hotel.Address,
+                City = hotel.City,
+                PostalCode = hotel.PostalCode,
+                State = hotel.State,
+                Country = hotel.Country,
+                CostPerNight = hotel.CostPerNight,
+                Status = hotel.Status,
+                StarRating = hotel.StarRating,
+                RoomType = hotel.RoomType,
+                NumberOfRooms = hotel.NumberOfRooms,
+                MaxOccupancy = hotel.MaxOccupancy
+            };
+
+            _context.Add(hotel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(hotel);
+    }
+
 }
 
--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -11,7 +11,13 @@ namespace Assign1.Models
         public string HotelName { get; set; }
 
         [Required]
+        public string Address { get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
 
         [Required]
         public string State { get; set; }
