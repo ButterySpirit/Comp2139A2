69893915e0828e58dd8b39e75613a8bb0ddbbcb0
IanM21 <ianmcdonad2003@gmail.com>
2024-02-14 13:34:00 -0500

    Added Create Account & Login functionality

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,19 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Assign1.Data;
+using Assign1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 
 namespace Assign1.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _context.Users
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(u => u.Username == model.Username);
+
+                if (user != null)
+                {
+                    if (user.Password == model.Password)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+            return View("Index", model);
+        }
     }
 }
+
 
--- Models/User.cs
+++ Models/User.cs
@@ -27,4 +27,15 @@ namespace Assign1.Models
             Bookings = new HashSet<Booking>();
         }
     }
+
+    public class Login
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
 }
