4035f1c623163e02bc2e5e38bf4b44b1d1db0502
IanM21 <ianmcdonad2003@gmail.com>
2024-02-16 11:45:05 -0500

    Merging Branches To Main Fixed Booking Total Cost Issues

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -1,20 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Assign1.Models;
+using Assign1.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Assign1.Controllers
 {
     public class BookingController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public BookingController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        // GET: Booking/Create
+        public IActionResult Create(int? hotelId, decimal? costPerNight)
+        {
+            if (hotelId.HasValue)
+            {
+                ViewBag.HotelId = hotelId.Value;
+            }
+            if (costPerNight.HasValue)
+            {
+                ViewBag.CostPerNight = costPerNight.Value;
+            }
+
             return View();
+        }
+
+
+        // POST: Booking/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,TotalCost,BookingDate,PaymentStatus")] Booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(booking);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Confirmation), new { id = booking.BookingId });
+            }
+            return View(booking);
+        }
+
+        // GET: Booking/Confirmation/{id}
+        public async Task<IActionResult> Confirmation(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings.FirstOrDefaultAsync(m => m.BookingId == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
         }
     }
 }
-
